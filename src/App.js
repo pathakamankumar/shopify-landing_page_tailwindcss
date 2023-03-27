d1fe28784416e6d4d1e91ea4c4aa192514b4f01e
aman kumar <aman.22@tecorb.co>
2023-03-27 15:56:40 +0530

    support Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import GrowBusiness from './Component/growBusiness';
 import Footer from './Component/footer';
+import Support from './Component/support'
 
 function App() {
   return (
     <>
       <GrowBusiness />
       <Footer/>
+      <Support/>
     </>
   );
 }
---
+++ src/Component/support/index.jsx
@@ -0,0 +1,8 @@
+const Support=()=>{
+    return(
+        <>
+            <p className="bg-purple-900 text-center">Support Section</p>
+        </>
+    )
+}
+export default Support;
